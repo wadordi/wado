b077f1de4a367fea01aeb938852eb95901cd8221
wadordi <tera15@docomo.ne.jp>
2020-12-02 13:07:25 +0000

    ４つくるくる

--- myled.c
+++ myled.c
@@ -24,7 +24,7 @@ static volatile u32 *gpio_base = NULL;
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	char c;
-	int i;
+	int a,i;
 	if(copy_from_user(&c,buf,sizeof(char)))
 	return -EFAULT;
 
@@ -84,7 +84,38 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 		msleep(200);
 		gpio_base[10] = 1 << 24;
 	}
-
+	/*else if(c == 'a'){
+		for(i=0;i < 8; i++){
+		a = rand()% 4 + 1;
+		if(a==1){
+			gpio_base[7] = 1 << 25;
+			msleep(50);
+			gpio_base[10] = 1 <<25;
+		       msleep(50);	
+			}
+		
+		if(a==2){
+			gpio_base[7] = 1 << 24;
+			msleep(50);
+			gpio_base[10] = 1 <<24;
+		       	msleep(50);
+			}
+		
+		if(a==3){
+			gpio_base[7] = 1 << 23;
+			msleep(50);
+			gpio_base[10] = 1 <<23;
+			msleep(50);	
+			}
+	
+		if(a==4){
+			gpio_base[7] = 1 << 22;
+			msleep(50);
+			gpio_base[10] = 1 <<22;
+		        msleep(50);	
+			}
+		}
+		}*/
 	return 1;
 }
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t*pos)
